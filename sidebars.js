108bfc8f48ba25cc02322c11bc9709caac476fbe
DarkSolarLegend <100734258+DarkSolarLegend@users.noreply.github.com>
2025-12-20 12:06:19 -0600

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -65,7 +65,8 @@ const sidebars = {
     'gallery/pittinteriors',
   ],
   changelogSidebar: [
-    'changelog/1.0.0.1',
+   'changelog/1.0.0.2',
+   'changelog/1.0.0.1',
     'changelog/1.0.0.0'
   ],
    resourcesSidebar: [
